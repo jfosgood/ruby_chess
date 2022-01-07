362537ec8436bfaa380bd03422a63710797a1d53
jfosgood <osgood.jake1@gmail.com>
2022-01-07 18:42:23 +0000

    Created base class for chess pieces

---
+++ lib/chess/piece.rb
@@ -0,0 +1,23 @@
+module Chess
+  class Piece 
+    attr_reader :color, :type
+    def initialize(color, type)
+      @color = color 
+      @type = type
+    end 
+    
+    def initial_type
+      if @type == :knight
+        'n'
+      else
+        type.to_s[0,1]
+      end 
+    end
+    
+    def ==(other)
+      return false unless other.respond_to?(:color)
+      (@tyoe == other.type and @color == other.color)
+    end 
+    
+  end
+end
