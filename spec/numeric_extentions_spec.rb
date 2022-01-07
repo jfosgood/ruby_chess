7673fb538439025cf82b950593ed667f0aef0dbd
jfosgood <osgood.jake1@gmail.com>
2022-01-07 18:36:41 +0000

    Added tests for numeric extentions

---
+++ spec/numeric_extentions_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+describe Numeric do 
+  context "#sign" do 
+    it "returns 1 for positive number" do 
+      number = 5
+      expect(number.sign).to eq(1)
+    end
+    
+    it "returns -1 for negative number" do 
+      number = -3
+      expect(number.sign).to eq(-1)
+    end
+    
+    it "returns 0 for 0" do 
+      zero = 0
+      expect(zero.sign).to eq(0)
+    end
+    
+  end 
+end 
