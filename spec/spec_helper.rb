d599d7150f5118bcf90b3be1f797c2d01e11de8f
jfosgood <osgood.jake1@gmail.com>
2022-01-07 18:33:41 +0000

    require numeric_extentions.rb in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "bundler/setup"
 require "chess"
 require_relative "../lib/chess.rb"
+require_relative "../lib/chess/numeric_extentions.rb"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
